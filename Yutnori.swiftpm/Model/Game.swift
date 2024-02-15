c0c94a8727ae594e16ae59e06327528841f629a9
eemdeeks <prince9088@gmail.com>
2024-02-15 19:03:17 +0900

    Add Game Model

---
+++ Yutnori.swiftpm/Model/Game.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by 박승찬 on 2/15/24.
+//
+
+import Foundation
+
+struct Game {
+    var turn: Team
+    var yut: Yut
+}
+
+enum Team {
+    case Red
+    case Blue
+
+    func switchingTeam() -> Team {
+        switch self {
+        case .Red:
+            return .Blue
+        case .Blue:
+            return .Red
+        }
+    }
+}
