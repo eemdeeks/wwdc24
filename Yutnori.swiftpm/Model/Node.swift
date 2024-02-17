8517025d0f627fade65d14823e99c4c6e5347fe6
eemdeeks <prince9088@gmail.com>
2024-02-17 21:36:55 +0900

    Update Node to test

--- Yutnori.swiftpm/Model/Node.swift
+++ Yutnori.swiftpm/Model/Node.swift
@@ -18,19 +18,22 @@ class Node<T>{
 }
 
 struct Nodes {
+    let start: Node<Offset>
     let block1: Node<Offset>
     let block2: Node<Offset>
     let block3: Node<Offset>
     let end: Node<Offset>
     init() {
-        self.block1 = Node(data: Offset(width: 0, height: 0))
-        self.block2 = Node(data: Offset(width: 0, height: 50))
-        self.block3 = Node(data: Offset(width: 0, height: 100))
-        self.end = Node(data: Offset(width: 0, height: 150))
+        self.start = Node(data: Offset(width: 0, height: 0))
+        self.block1 = Node(data: Offset(width: 0, height: 80))
+        self.block2 = Node(data: Offset(width: 0, height: 160))
+        self.block3 = Node(data: Offset(width: 0, height: 320))
+        self.end = Node(data: Offset(width: 80, height: 320))
 
+        start.next = block1
         block1.next = block2
         block2.next = block3
         block3.next = end
-        end.next = block1
+        end.next = start
     }
 }
