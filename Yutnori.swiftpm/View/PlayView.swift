4fd87bd076d0c8d352b673f2412933489c7f4b5c
eemdeeks <prince9088@gmail.com>
2024-02-20 18:02:11 +0900

    Add field

--- Yutnori.swiftpm/View/PlayView.swift
+++ Yutnori.swiftpm/View/PlayView.swift
@@ -11,40 +11,49 @@ struct PlayView: View {
     @StateObject var viewModel = PlayViewModel()
 
     var body: some View {
-        VStack {
-            Text("\(viewModel.game.action.rawValue)")
-            Text("\(viewModel.game.yut.rawValue)")
-            Text("\(viewModel.game.turn.rawValue)")
-            ZStack {
-                ForEach(viewModel.game.redPieces.indices, id: \.self) { index in
-                    Image("RedPiece")
-                        .offset(CGSize(width: viewModel.game.redPieces[index].offset.width, height: viewModel.game.redPieces[index].offset.height))
-                        .animation(.easeInOut, value: viewModel.game.redPieces[index].offset)
-                        .onTapGesture {
-                            if viewModel.game.turn == .Red {
-                                viewModel.choosePiece(movePieceIndex: index)
+        ZStack {
+            HStack {
+                Image("field")
+                    .resizable()
+                    .frame(height: UIScreen.main.bounds.height)
+                    .scaledToFit()
+                Spacer()
+            }
+            VStack {
+                Text("\(viewModel.game.action.rawValue)")
+                Text("\(viewModel.game.yut.rawValue)")
+                Text("\(viewModel.game.turn.rawValue)")
+                ZStack {
+                    ForEach(viewModel.game.redPieces.indices, id: \.self) { index in
+                        Image("RedPiece")
+                            .offset(CGSize(width: viewModel.game.redPieces[index].offset.width, height: viewModel.game.redPieces[index].offset.height))
+                            .animation(.easeInOut, value: viewModel.game.redPieces[index].offset)
+                            .onTapGesture {
+                                if viewModel.game.turn == .Red {
+                                    viewModel.choosePiece(movePieceIndex: index)
+                                }
                             }
-                        }
 
+                    }
                 }
-            }
-            .padding()
-            ZStack {
-                ForEach(viewModel.game.bluePieces.indices, id: \.self) { index in
-                    Image("BluePiece")
-                        .offset(CGSize(width: viewModel.game.bluePieces[index].offset.width, height: viewModel.game.bluePieces[index].offset.height))
-                        .animation(.easeInOut, value: viewModel.game.bluePieces[index].offset)
-                        .onTapGesture {
-                            if viewModel.game.turn == .Blue {
-                                viewModel.choosePiece(movePieceIndex: index)
+                .padding()
+                ZStack {
+                    ForEach(viewModel.game.bluePieces.indices, id: \.self) { index in
+                        Image("BluePiece")
+                            .offset(CGSize(width: viewModel.game.bluePieces[index].offset.width, height: viewModel.game.bluePieces[index].offset.height))
+                            .animation(.easeInOut, value: viewModel.game.bluePieces[index].offset)
+                            .onTapGesture {
+                                if viewModel.game.turn == .Blue {
+                                    viewModel.choosePiece(movePieceIndex: index)
+                                }
                             }
-                        }
+                    }
                 }
-            }
-            Button("윷 굴리기") {
-                viewModel.throwYut()
-            }
+                Button("윷 굴리기") {
+                    viewModel.throwYut()
+                }
 
+            }
         }
     }
 }
