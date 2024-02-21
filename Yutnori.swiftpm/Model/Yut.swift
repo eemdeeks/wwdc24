95f94dc2b50061c447f13716287491d7173d690f
eemdeeks <prince9088@gmail.com>
2024-02-21 17:01:47 +0900

    Feat Yut probability

--- Yutnori.swiftpm/Model/Yut.swift
+++ Yutnori.swiftpm/Model/Yut.swift
@@ -8,24 +8,39 @@
 import Foundation
 
 enum Yut: String, CaseIterable {
-    case Do = "Do"
-    case Gae = "Gae"
-    case Geol = "Geol"
-    case Yut = "Yut"
-    case Mo = "Mo"
+    case Do, Do1, Do2, Do3
+    case Gae, Gae1, Gae2, Gae3, Gae4, Gae5
+    case Geol, Geol1, Geol2, Geol3
+    case Yut
+    case Mo
 
     var number: Int {
         switch self {
-        case .Do:
+        case .Do, .Do1, .Do2, .Do3:
             1
-        case .Gae:
+        case .Gae, .Gae1, .Gae2, .Gae3, .Gae4, .Gae5:
             2
-        case .Geol:
+        case .Geol, .Geol1, .Geol2, .Geol3:
             3
         case .Yut:
             4
         case .Mo:
             5
+        }
+    }
+
+    var name: String {
+        switch self {
+        case .Do, .Do1, .Do2, .Do3:
+            "Do"
+        case .Gae, .Gae1, .Gae2, .Gae3, .Gae4, .Gae5:
+            "Gae"
+        case .Geol, .Geol1, .Geol2, .Geol3:
+            "Geol"
+        case .Yut:
+            "Yut"
+        case .Mo:
+            "Mo"
         }
     }
 }
