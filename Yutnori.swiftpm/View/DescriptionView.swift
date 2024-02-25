4e9989b0efd85902e7df7ff5e8b57979f8d86e28
eemdeeks <prince9088@gmail.com>
2024-02-25 23:49:10 +0900

    add DescriptionView

---
+++ Yutnori.swiftpm/View/DescriptionView.swift
@@ -0,0 +1,27 @@
+//
+//  DescriptionView.swift
+//
+//
+//  Created by 박승찬 on 2/25/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct DescriptionView: View {
+    var body: some View {
+        ScrollView(.vertical) {
+        }
+    }
+}
+
+extension DescriptionView {
+    private var header: some View {
+        Text("Yutnori")
+            .font(.custom(.dovemayo, size: 60))
+    }
+
+    private var descriptionYutnori: some View {
+        
+    }
+}
